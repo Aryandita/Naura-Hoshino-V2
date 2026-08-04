676826b701e44a44ab4efef074e52118974a3f44
Aryandita Praftian <paryandita@gmail.com>
2026-08-05 06:02:48 +0800

    refactor(ui): pisahkan emoji media, musik, dan utilitas

---
+++ src/config/ui/emojis_media.js
@@ -0,0 +1,112 @@
+'use strict';
+
+// Emoji downloader, utilitas (anime, film, cuaca), kontrol musik, pencapaian,
+// minigame lokasi, clan, gacha, dan cerita.
+
+module.exports = {
+    // --- Downloader ---
+    tiktok: '<:tiktok:1547565608970203176>',
+    instagram: '<:Instagram:1532358955165024256>',
+    twitter: '<:X_:1532358957048401950>',
+    facebook: '<:Facebook:1532358953344700437>',
+    threads: '<:Threads:1532358951528562860>',
+    download: '<a:download:1525035127611064431>',
+    camera_with_flash: '\uD83D\uDCF8',
+    clapper: '\uD83C\uDFAC',
+    music_note: '\uD83C\uDFB5',
+    bird: '\uD83D\uDC26',
+    pushpin: '\uD83D\uDCCC',
+    robot: '\uD83E\uDD16',
+    thread: '\uD83E\uDDF5',
+    globe_with_meridians: '\uD83C\uDF10',
+    tv: '\uD83D\uDCFA',
+
+    // --- Utility Commands (Anime, Movie, Weather) ---
+    anime_search: '<:search:1525032346406096946>',
+    anime_rating: '<a:rate:1525032352793890896>',
+    anime_type: '<a:SpinningBook:1492696903069208627>',
+    anime_episodes: '<a:clock:1492712296697298954>',
+    anime_status: '<a:information:1499384250221199360>',
+    anime_genres: '<a:movie:1525032349434249307>',
+    anime_age_rating: '<:web:1492712301986447511>',
+    movie_search: '<:search:1525032346406096946>',
+    movie_director: '<a:movie:1525032349434249307>',
+    movie_actors: '<a:movie:1525032349434249307>',
+    movie_rating: '<a:rate:1525032352793890896>',
+    movie_runtime: '<a:clock:1492712296697298954>',
+    movie_awards: '<a:gold:1492731787137519666>',
+    weather_temp: '<:temp:1525032365229998080>',
+    weather_humidity: '<a:humidity:1525033136323301376>',
+    weather_wind: '<a:wind:1525032360322535484>',
+    weather_temp_max: '<:temp:1525032365229998080>',
+    weather_temp_min: '<:temp:1525032365229998080>',
+    weather_sunny: '<a:sunmove:1500705371134889995>',
+    weather_mainly_clear: '<a:sunmove:1500705371134889995>',
+    weather_cloudy: '<a:summer:1500721239054876802>',
+    weather_overcast: '<a:summer:1500721239054876802>',
+    weather_fog: '<a:wind:1525032360322535484>',
+    weather_rain: '<a:255591blueumbrella:1523226263844556882>',
+    weather_snow: '<a:6960snowfall:1523226262183612436>',
+    weather_thunder: '<a:thunder:1525032363225125046>',
+
+    // --- Music Controls & Info ---
+    nowplaying: '<a:DiscSpinner1:1492696912145678488>',
+    progressDot: '<:DotMusic:1488166056768835596>',
+    progressLineBefore: '<:AfterDot:1488166236004159509>',
+    progressLineAfter: '<:BeforeDot:1488166108081950882>',
+    favorite: '<a:SpinHeart:1492696848643915796>',
+    filter: '<:Filter:1484705994020753529>',
+    musicListener: '<a:Listener:1492696916050444449>',
+    musicArtist: '<:Artis:1484706029244518561>',
+    musicPlayPause: '<:PlayPause:1484705975998091375>',
+    musicSkip: '<:Skip:1484705981152755712>',
+    musicStop: '<:Stop:1484705983778525315>',
+    musicLoop: '<:Loop:1484705967991034010>',
+    musicVolDown: '<:VolumeDown:1484874588524646621>',
+    musicVolUp: '<:VolumeUp:1484874537110864034>',
+    musicAutoplay: '<:AutoPlay:1484705985980268744>',
+    musicLyrics: '<:Lyrics:1484705972919337070>',
+    musicShuffle: '<:Shuffle:1484705970469867641>',
+    music247: '<a:Moon:1492696850602524682>',
+
+    // --- Music Filters (DSP) ---
+    normal: '<:MusicDisc:1484706066662031483>',
+    bassboost: '<:BassBoost:1493476326634684517>',
+    nightcore: '<:NightCore:1493476329658515497>',
+    vaporwave: '<:vaporwave:1493478233448910878>',
+
+    // --- Music Sources ---
+    spotify: '<:Spotify:1500311186736939008>',
+    youtube: '<a:Youtube:1500310258709434448>',
+    soundcloud: '<:SoundCloud:1500310263247671347>',
+
+    // --- Quest ---
+    quest_star: '<a:gold:1492731787137519666>',
+    quest_clock: '<a:clock:1492712296697298954>',
+
+    // --- Achievements & Titles ---
+    achievement_badge: '\uD83C\uDFC5',
+    achievement_locked: '\uD83D\uDD12',
+    achievement_unlocked: '\uD83D\uDD13',
+
+    // --- Location Minigames ---
+    fishing_net: '\uD83C\uDFA3',
+    fish_rare: '\uD83D\uDC21',
+    fish_legendary: '\uD83E\uDD88',
+    wood_oak: '\uD83E\uDEB5',
+    wood_mahogany: '\uD83C\uDF33',
+    ore_gold: '\uD83D\uDFE1',
+    ore_ruby: '\uD83D\uDD34',
+
+    // --- Guild & Co-op ---
+    guild_banner: '\uD83C\uDF8C',
+    guild_shield: '\uD83D\uDEE1\uFE0F',
+
+    // --- Gacha ---
+    gacha_pull: '\uD83C\uDFB0',
+    gacha_legendary: '\u2728',
+
+    // --- Story Quest ---
+    story_scroll: '\uD83D\uDCDC',
+    story_book: '\uD83D\uDCD6'
+};
